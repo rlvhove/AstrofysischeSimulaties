bbbfd98c893228a6965ae4c72d3dab1d8a2766e5
Robijn.Vanhove@ugent.be <Robijn@MacBook-Pro-van-Robijn.local>
2015-10-23 15:21:29 +0200

    Initial Commit

---
+++ AstrofysischeSimulaties/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AstrofysischeSimulaties
+//
+//  Created by Robijn Vanhove on 23/10/15.
+//  Copyright (c) 2015 company. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
